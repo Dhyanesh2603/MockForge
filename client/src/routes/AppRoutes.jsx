51a5c2d14cb45c415ce4da7a72368ad52243b264
dhyanesh-2603 <dhyanesh450@gmail.com>
2026-05-28 09:27:41 +0530

    Initial setup: project structure, frontend & backend base

---
+++ client/src/routes/AppRoutes.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import HomePage from "../pages/HomePage";
+import LoginPage from "../pages/auth/LoginPage";
+import DashboardPage from "../pages/dashboard/DashboardPage";
+
+function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+
+        <Route path="/login" element={<LoginPage />} />
+
+        <Route path="/dashboard" element={<DashboardPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default AppRoutes;
