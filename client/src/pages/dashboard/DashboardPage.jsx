fd9f66e80ba8a297af0cbb9f64f1f7fce00950f3
dhyanesh-2603 <dhyanesh450@gmail.com>
2026-05-28 10:35:55 +0530

    Setup Firebase authentication frontend

--- client/src/pages/dashboard/DashboardPage.jsx
+++ client/src/pages/dashboard/DashboardPage.jsx
@@ -1,9 +1,25 @@
+import { useAuth } from "../../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
 function DashboardPage() {
+  const { user, loading } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!loading && !user) {
+      navigate("/login");
+    }
+  }, [user, loading]);
+
+  if (loading) return <div>Loading...</div>;
+
   return (
     <div className="flex h-screen items-center justify-center">
-      <h1 className="text-3xl font-bold">
-        Dashboard
-      </h1>
+      <div>
+        <h1 className="text-2xl font-bold">Dashboard</h1>
+        <p>Welcome {user?.displayName}</p>
+      </div>
     </div>
   );
 }
